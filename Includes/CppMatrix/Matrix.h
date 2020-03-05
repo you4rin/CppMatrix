2f0ef2669c41594a788316ae6647ba3583ba93b4
you4rin <artichoke42@naver.com>
2020-03-05 16:18:14 +0900

    Remove Matrix.cpp

--- Includes/CppMatrix/Matrix.h
+++ Includes/CppMatrix/Matrix.h
@@ -1,4 +1,5 @@
 #include<vector>
+#include<complex>
 /*
 무엇을 구현해야 하는가?
 1. 각종 연산자(특히 곱셈의 경우, 시간복잡도 O(N^2.3)인 방법 찾아보도록 하자)
@@ -12,16 +13,44 @@ class Matrix{
 	std::vector<T> matrix;
 public:
 	//인자 없음
-	Matrix();
+	Matrix():h(1),w(1),matrix(std::vector<T>(1,0)){}
 	//인자로 h와 w만 받음, 0으로 초기화
-	Matrix(int h,int w);
+	Matrix(int h,int w):h(h),w(w),matrix(std::vector<T>(h*w,0)){}
 	//Initializer_list를 이용한 Matrix 초기화 구현 필요
-	Matrix<T>& operator=(const Matrix<T>& mat);
-	Matrix<T>& operator=(Matrix<T>&& mat);
-	Matrix<T> operator+(const Matrix<T>& mat);
-	Matrix<T> operator+(Matrix<T>&& mat);
-	Matrix<T> operator-(const Matrix<T>& mat);
-	Matrix<T> operator-(Matrix<T>&& mat);
-	Matrix<T> operator*(const Matrix<T>& mat);
-	Matrix<T> operator*(Matrix<T>&& mat);
+	Matrix<T>& operator=(const Matrix<T>& mat){
+
+	}
+	Matrix<T>& operator=(Matrix<T>&& mat){
+
+	}
+	Matrix<T> operator+(const Matrix<T>& mat){
+
+	}
+	Matrix<T> operator+(Matrix<T>&& mat){
+
+	}
+	Matrix<T> operator-(const Matrix<T>& mat){
+
+	}
+	Matrix<T> operator-(Matrix<T>&& mat){
+
+	}
+	Matrix<T> operator*(const Matrix<T>& mat){
+
+	}
+	Matrix<T> operator*(Matrix<T>&& mat){
+
+	}
 };
+template<> 
+Matrix<float> Matrix<int>::operator+(const Matrix<float>& mat){
+
+}
+template<> 
+Matrix<float> Matrix<int>::operator+(Matrix<float>&& mat){
+
+}
+template<> 
+Matrix<float> Matrix<float>::operator+(Matrix<int>&& mat){
+
+}
--- Sources/CppMatrix/Matrix.cpp
+++
@@ -1,7 +0,0 @@
-#include<CppMatrix/Matrix.h>
-
-template<class T>
-Matrix<T>::Matrix():h(1),w(1),matrix(std::vector<T>(1,0)()){}
-
-template<class T>
-Matrix<T>::Matrix(int h,int w):h(h),w(w),matrix(std::vector<T>(h*w,0)()){}
