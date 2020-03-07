f94962844f9d0bd24e6a0484d4fb03bfb3a1e3c5
you4rin <artichoke42@naver.com>
2020-03-07 22:19:35 +0900

    define operators(except multiplication)

--- Includes/CppMatrix/Matrix.h
+++ Includes/CppMatrix/Matrix.h
@@ -17,40 +17,261 @@ public:
 	//인자로 h와 w만 받음, 0으로 초기화
 	Matrix(int h,int w):h(h),w(w),matrix(std::vector<T>(h*w,0)){}
 	//Initializer_list를 이용한 Matrix 초기화 구현 필요
-	Matrix<T>& operator=(const Matrix<T>& mat){
-
+	//일반적인 Matrix간의 대입연산
+	template<class U>
+	Matrix<T>& operator=(const Matrix<U>& other){
+		if(h!=other.h||w!=other.w){
+			throw std::exception;
+		}
+		for(int i=0;i<h*w;++i){
+			matrix[i]=other.matrix[i];
+		}
+		return *this;
 	}
-	Matrix<T>& operator=(Matrix<T>&& mat){
 
+	template<class U>
+	Matrix<T>& operator=(Matrix<U>&& other){
+		if(h!=other.h||w!=other.w){
+			throw std::exception;
+		}
+		for(int i=0;i<h*w;++i){
+			matrix[i]=other.matrix[i];
+		}
+		return *this;
 	}
-	Matrix<T> operator+(const Matrix<T>& mat){
 
+	//일반적인 Matrix간의 덧셈
+	template<class U>
+	Matrix<T> operator+(const Matrix<U>& other){
+		if(h!=other.h||w!=other.w){
+			throw std::exception;
+		}
+		Matrix<T> mat(h,w);
+		for(int i=0;i<h*w;++i){
+			mat.matrix[i]=matrix[i]+other.matrix[i];
+		}
+		return mat;
 	}
-	Matrix<T> operator+(Matrix<T>&& mat){
 
+	template<class U>
+	Matrix<T> operator+(Matrix<U>&& other){
+		if(h!=other.h||w!=other.w){
+			throw std::exception;
+		}
+		Matrix<T> mat(h,w);
+		for(int i=0;i<h*w;++i){
+			mat.matrix[i]=matrix[i]+other.matrix[i];
+		}
+		return mat;
 	}
-	Matrix<T> operator-(const Matrix<T>& mat){
 
+	//일반적인 Matrix간의 뺄셈
+	template<class U>
+	Matrix<T> operator-(const Matrix<U>& other){
+		if(h!=other.h||w!=other.w){
+			throw std::exception;
+		}
+		Matrix<T> mat(h,w);
+		for(int i=0;i<h*w;++i){
+			mat.matrix[i]=matrix[i]-other.matrix[i];
+		}
+		return mat;
 	}
-	Matrix<T> operator-(Matrix<T>&& mat){
 
+	template<class U>
+	Matrix<T> operator-(Matrix<U>&& other){
+		if(h!=other.h||w!=other.w){
+			throw std::exception;
+		}
+		Matrix<T> mat(h,w);
+		for(int i=0;i<h*w;++i){
+			mat.matrix[i]=matrix[i]-other.matrix[i];
+		}
+		return mat;
 	}
-	Matrix<T> operator*(const Matrix<T>& mat){
 
+	//일반적인 Matrix간의 곱셈 -> 추후 구현 필요
+	template<class U>
+	Matrix<T> operator*(const Matrix<U>& other){
+		if(w!=other.h){
+			throw std::exception;
+		}
 	}
-	Matrix<T> operator*(Matrix<T>&& mat){
 
+	template<class U>
+	Matrix<T> operator*(Matrix<U>&& other){
+		if(w!=other.h){
+			throw std::exception;
+		}
 	}
 };
-template<> 
-Matrix<float> Matrix<int>::operator+(const Matrix<float>& mat){
 
+template<> 
+Matrix<float> Matrix<int>::operator+(const Matrix<float>& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<float> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=other.matrix[i]+matrix[i];
+	}
+	return mat;
 }
-template<> 
-Matrix<float> Matrix<int>::operator+(Matrix<float>&& mat){
 
+template<> 
+Matrix<float> Matrix<int>::operator+(Matrix<float>&& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<float> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=other.matrix[i]+matrix[i];
+	}
+	return mat;
 }
-template<> 
-Matrix<float> Matrix<float>::operator+(Matrix<int>&& mat){
 
+template<> 
+Matrix<float> Matrix<int>::operator-(const Matrix<float>& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<float> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=-other.matrix[i]+matrix[i];
+	}
+	return mat;
+}
+
+template<> 
+Matrix<float> Matrix<int>::operator-(Matrix<float>&& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<float> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=-other.matrix[i]+matrix[i];
+	}
+	return mat;
+}
+
+template<> 
+Matrix<double> Matrix<int>::operator+(const Matrix<double>& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<double> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=other.matrix[i]+matrix[i];
+	}
+	return mat;
+}
+
+template<> 
+Matrix<double> Matrix<int>::operator+(Matrix<double>&& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<double> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=other.matrix[i]+matrix[i];
+	}
+	return mat;
+}
+
+template<> 
+Matrix<double> Matrix<int>::operator-(const Matrix<double>& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<double> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=-other.matrix[i]+matrix[i];
+	}
+	return mat;
+}
+
+template<> 
+Matrix<double> Matrix<int>::operator-(Matrix<double>&& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<double> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=-other.matrix[i]+matrix[i];
+	}
+	return mat;
+}
+
+template<> 
+Matrix<long double> Matrix<int>::operator+(const Matrix<long double>& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<long double> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=other.matrix[i]+matrix[i];
+	}
+	return mat;
+}
+
+template<> 
+Matrix<long double> Matrix<int>::operator+(Matrix<long double>&& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<long double> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=other.matrix[i]+matrix[i];
+	}
+	return mat;
+}
+
+template<> 
+Matrix<long double> Matrix<int>::operator-(const Matrix<long double>& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<long double> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=-other.matrix[i]+matrix[i];
+	}
+	return mat;
+}
+
+template<> 
+Matrix<long double> Matrix<int>::operator-(Matrix<long double>&& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<long double> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=-other.matrix[i]+matrix[i];
+	}
+	return mat;
+}
+
+template<> 
+template<class T>
+Matrix<std::complex<T>> Matrix<int>::operator+(Matrix<std::complex<T>>&& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<std::complex<T>> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=other.matrix[i]+matrix[i];
+	}
+	return mat;
+}
+
+template<> 
+template<class T>
+Matrix<std::complex<T>> Matrix<int>::operator-(Matrix<std::complex<T>>&& other){
+	if(h!=other.h||w!=other.w){
+		throw std::exception;
+	}
+	Matrix<std::complex<T>> mat(h,w);
+	for(int i=0;i<h*w;++i){
+		mat.matrix[i]=-other.matrix[i]+matrix[i];
+	}
+	return mat;
 }
